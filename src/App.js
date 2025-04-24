926907f8f94c6862edea8814e44bd0857b38a200
Higor Silva <higor.silva14@aluno.senai.br>
2025-04-24 08:25:11 -0300

    Feat: Iniciando o React.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//chamar as telas do index.js
+import './App.css';
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme';
+
+// import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <>
+     <CadastroFilme/>
+     {/* // <Login/> */}
+    </>
+   
+
+  );
+}
+//export : exporta o codigo de forma padrao
+export default App;
