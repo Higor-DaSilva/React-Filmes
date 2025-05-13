ae160f5bbdc43f299e5310641a0379e749c13b29
Higor Silva <higor.silva14@aluno.senai.br>
2025-05-13 16:44:55 -0300

    Feat: Começando o consumo de Api no React

--- src/components/cadastro/Cadastro.jsx
+++ src/components/cadastro/Cadastro.jsx
@@ -4,13 +4,22 @@ import Botao from "../botao/Botao"
 const Cadastro = (props) => {
     return (
         <section className="section_cadastro">
-            <form action="" className="layout_grid form_cadastro">
+            <form onSubmit={props.funcCadastro} className="layout_grid form_cadastro">
                 <h1>{props.tituloCadastro}</h1>
                 <hr />
                 <div className="campos_cadastro">
                     <div className="campo_cad_nome">
                         <label htmlFor="">Nome</label>
-                        <input type="text" name="nome" placeholder={`Digite o nome do ${props.campoPlaceholder}`} />
+                        <input 
+                            type="text" 
+                            name="nome" 
+                            placeholder={`Digite o nome do ${props.campoPlaceholder}`} 
+                            value={props.valorInput}
+                            //ao mudar o input algo acontece
+                                //atualizar o estado do pai ao digitar
+                                //targer está indo buscar o valor do "e"
+                            onChange={(e) => props.setValorInput(e.target.value)}
+                        />
                     </div>
                     <div className="campo_cad_genero" style={{display:props.visibilidade}}>
                         <label htmlFor="genero">Gênero</label>
--- src/pages/cadastroGenero/CadastroGenero.jsx
+++ src/pages/cadastroGenero/CadastroGenero.jsx
@@ -1,25 +1,88 @@
+import { useState } from "react";
+import api from "../../Services/services"
+
+//importar o sweet alert    
+import Swal from 'sweetalert2'
+
+//importação de componentes:
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import Cadastro from "../../components/cadastro/Cadastro";
 import Lista from "../../components/lista/Lista";
+
 const CadastroGenero = () => {
-    return(
-       <>
-            <Header/>
+
+    //nome do genero
+    const [genero, setGenero] = useState("");
+
+    function alerta(icone, mesagem){
+        const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+});
+Toast.fire({
+  icon: icone,
+  title: mesagem
+});
+    }
+
+    async function cadastrarGenero(evt) {
+        evt.preventDefault();
+        //verificar se o input está vindo vazio
+        if (genero.trim() != "") {
+            //try => tentar(o esperado)
+            //catch => lança a exceção
+            try {
+                //cadastrar um gènero: post
+                await api.post("genero", { nome: genero });
+                alerta("success", "Cadastro realizado com sucesso")
+                setGenero("");
+            } catch (error) {
+               
+                alerta("error", "Erro! entre em contato com o suporte!")
+                console.log(error);
+            }
+        } else {
+        alerta("warning", "Preencha o campo")
+        }
+    }
+
+    return (
+        <>
+            <Header />
             <main>
-                <Cadastro 
-                tituloCadastro="Cadastro de Gênero"
-                visibilidade="none"
-                campoPlaceholder="Gênero"
+                <Cadastro
+                    tituloCadastro="Cadastro de Gênero"
+                    visibilidade="none"
+                    campoPlaceholder="Gênero"
+                    //atribuindo a função:
+                    funcCadastro={cadastrarGenero}
+                    //atribuindo o valor do input:
+                    valorInput={genero}
+                    //atribuindo a função que atualiza o meu genero:
+                    setValorInput={setGenero}
                 />
                 <Lista
-                 nomeLista="Lista de Gênero"
-                 visi_lista="none"
-                 />
+                    nomeLista="Lista de Gênero"
+                    visi_lista="none"
+                />
             </main>
-            <Footer/>
-       </>
+            <Footer />
+        </>
     )
 }
 
 export default CadastroGenero;
+
+// teste: validar o  genero
+//     useEffect(() => {
+//         console.log(genero);
+//     }, [genero]);
+// fim do teste
